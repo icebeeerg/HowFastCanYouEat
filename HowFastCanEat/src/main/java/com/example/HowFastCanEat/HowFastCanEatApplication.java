079d54e0766844959b2692d3291a26effe47b010
icebeeerg <123386813+icebeeerg@users.noreply.github.com>
2023-02-28 10:35:15 +0100

    Add files via upload

---
+++ HowFastCanEat/src/main/java/com/example/HowFastCanEat/HowFastCanEatApplication.java
@@ -0,0 +1,13 @@
+package com.example.HowFastCanEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowFastCanEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowFastCanEatApplication.class, args);
+	}
+
+}
---
+++ HowFastCanEat/src/test/java/com/example/HowFastCanEat/HowFastCanEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HowFastCanEat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowFastCanEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
